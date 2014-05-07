16bea667ac743e55cf76ad532033c6eaceac44c1
InoueKenta <NQuang17@gmail.com>
2014-05-07 17:04:56 +0700

    Update Scanline Beta (Chưa test)

---
+++ Core/SortedLinkedList.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TabletC.Core
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+            Nex = Pre = null;
+        }
+
+        public DoublyLinkedListNode<T> Next
+        {
+            get { return Nex; }
+        }
+
+        public DoublyLinkedListNode<T> Previous
+        {
+            get { return Pre; }
+        }
+
+        public T Value { get; set; }
+
+        internal DoublyLinkedListNode<T> Nex;
+        internal DoublyLinkedListNode<T> Pre;
+    }
+
+    public class SortedDoublyLinkedList<T> where T : IComparable<T>
+    {
+
+        public SortedDoublyLinkedList()
+        {
+            Count = 0;
+            _head = _tail = null;
+        }
+
+        public int Count { get; private set; }
+
+        public DoublyLinkedListNode<T> First
+        {
+            get { return _head; }
+        }
+
+        public DoublyLinkedListNode<T> Last
+        {
+            get { return _tail; }
+        }
+
+
+        public void Add(T value)
+        {
+            Add(new DoublyLinkedListNode<T>(value));
+        }
+
+        public void Add(DoublyLinkedListNode<T> newNode)
+        {
+            if (_head == null)   // list is empty
+            {
+                _head = _tail = newNode;
+            }
+            else if (_head == _tail)  // list has one element
+            {
+                if (_head.Value.CompareTo(newNode.Value) <= 0)
+                {
+                    _tail = newNode;
+                }
+                else
+                {
+                    _head = newNode;
+                }
+                connect(_head, _tail);
+            }
+            else        // list has 2 or more elements
+            {
+                if (newNode.Value.CompareTo(_head.Value) <= 0)       // insert head
+                {
+                    connect(newNode, _head);
+                    _head = newNode;
+                }
+                else if (newNode.Value.CompareTo(_tail.Value) > 0)   // insert tail
+                {
+                    connect(_tail, newNode);
+                    _tail = newNode;
+                }
+                else                                                // somewhere in the middle
+                {
+                    var current = _tail.Pre;
+                    DoublyLinkedListNode<T> insertAfter = null;
+                    while (current != _head)
+                    {
+                        if (newNode.Value.CompareTo(current.Value) > 0)
+                        {
+                            insertAfter = current;
+                            break;
+                        }
+                        current = current.Pre;
+                    }
+
+                    if (insertAfter == null)
+                        insertAfter = _head;
+                    // disconnect(insertBefore.previous, insertBefore);     // do not, previous needed on the next line
+                    connect(newNode, insertAfter.Nex);
+                    connect(insertAfter, newNode);
+                }
+            }
+
+            Count++;
+        }
+
+        public void Remove(DoublyLinkedListNode<T> node)
+        {
+            if (node == _head)
+            {
+                // Remove head
+                _head = node.Next;
+                _head.Pre = null;
+            }
+            else if (node == _tail)
+            {
+                // Remove tail
+                _tail = node.Pre;
+                _tail.Nex = null;
+            }
+            else
+            {
+                // Remove middle
+                var pre = node.Pre;
+                var nex = node.Nex;
+                pre.Nex = nex;
+                nex.Pre = pre;
+            }
+            Count--;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
+
+        public void Sort()
+        {
+            var tlist = _head;
+
+            Clear();
+            while (tlist != null)
+            {
+                Add(tlist.Value);
+                tlist = tlist.Next;
+            }
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public class Enumerator : IEnumerator<T>
+        {
+            private SortedDoublyLinkedList<T> _list;
+            private DoublyLinkedListNode<T> _current;
+            private bool _start;
+
+            internal Enumerator(SortedDoublyLinkedList<T> list)
+            {
+                _list = list;
+                _current = null;
+                _start = true;
+            }
+
+            public void Reset()
+            {
+                _start = true;
+                _current = null;
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            // Summary:
+            //     Gets the element at the current position of the enumerator.
+            //
+            // Returns:
+            //     The element in the System.Collections.Generic.LinkedList<T> at the current
+            //     position of the enumerator.
+            public T Current
+            {
+                get { return _current.Value; }
+            }
+
+            // Summary:
+            //     Releases all resources used by the System.Collections.Generic.LinkedList<T>.Enumerator.
+            public void Dispose()
+            {
+            }
+
+            //
+            // Summary:
+            //     Advances the enumerator to the next element of the System.Collections.Generic.LinkedList<T>.
+            //
+            // Returns:
+            //     true if the enumerator was successfully advanced to the next element; false
+            //     if the enumerator has passed the end of the collection.
+            //
+            // Exceptions:
+            //   System.InvalidOperationException:
+            //     The collection was modified after the enumerator was created.
+            public bool MoveNext()
+            {
+                if (_start)
+                {
+                    // if (current == null) {      // circular
+                    _start = false;
+                    _current = _list._head;
+                }
+                else if (_current == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    _current = _current.Next;
+                }
+                return HasCurrent();
+            }
+
+            private bool HasCurrent()
+            {
+                return _current != null;
+            }
+        }
+
+        private void connect(DoublyLinkedListNode<T> n1, DoublyLinkedListNode<T> n2)
+        {
+            n1.Nex = n2;
+            n2.Pre = n1;
+        }
+
+        private DoublyLinkedListNode<T> _head;
+        private DoublyLinkedListNode<T> _tail;
+    }
+}
--- DrawPad/ShapeFiller.cs
+++ DrawPad/ShapeFiller.cs
@@ -77,6 +77,22 @@ namespace TabletC.DrawPad
         }
     }
 
+    internal class CActiveEdge : IComparable<CActiveEdge>
+    {
+        public int YUper { get; set; }
+        public double XIntersection { get; set; }
+        public double ReciSlope { get; set; }
+
+        public int CompareTo(CActiveEdge other)
+        {
+            if (XIntersection < other.XIntersection)
+                return -1;
+            if (XIntersection > other.XIntersection)
+                return 1;
+            return 0;
+        }
+    }
+
     class ShapeFiller
     {
         public void FillByFlood(Layer layer, IShape shape, Point? pstart)
@@ -132,6 +148,8 @@ namespace TabletC.DrawPad
                 ShapeDrawer.CreateShapeArea(shape.StartVertex, shape.EndVertex));
         }
 
+
+
         private void QueueFloodFill4(ref byte[] arr, ref Queue<Point> queue, int x, int y, int w, int h, int stride, CColor cfill, CColor cbound)
         {
             //don't go over the edge
@@ -151,6 +169,138 @@ namespace TabletC.DrawPad
                 queue.Enqueue(new Point(x - 1, y));
                 queue.Enqueue(new Point(x, y - 1));
             }
+        }
+
+        public void PaintByScanline(ref Layer layer, IShape shape)
+        {
+            
+            var colorFill = new CColor(((SolidBrush)shape.ShapeBrush).Color);
+            Color FillColor = Color.FromArgb(colorFill.A, colorFill.R, colorFill.G, colorFill.B);
+
+            switch (shape.GetShapeType())
+            {
+                case ShapeType.Rectangle:
+                case ShapeType.Polygon:
+                    ScanLineFillPolygon(ref layer, ref shape, FillColor);
+                    break;
+                case ShapeType.Circle:
+                    ScanLineFillCircle(ref layer, (Circle)shape, FillColor);
+                    break;
+                case ShapeType.Ellipse:
+                    ScanLineFillEllipse(ref layer, (Ellipse)shape, FillColor);
+                    break;
+            }
+
+
+        }
+
+        private void ScanLineFillPolygon(ref Layer layer,ref IShape shape,Color FillColor)
+        {
+            if ((shape.StartVertex.X == shape.EndVertex.X && shape.StartVertex.Y == shape.EndVertex.Y) ||
+                (shape.GetShapeType() == ShapeType.Polygon && shape.EndVertex.X != -1))
+                return;
+
+            var rec = Util.CreateBorder(shape);
+            var h = rec.Y + 1;
+            var et = new SortedDoublyLinkedList<CActiveEdge>[h];
+            var active = new SortedDoublyLinkedList<CActiveEdge>();
+
+            for (int i = 0; i < h; i++)
+                et[i] = new SortedDoublyLinkedList<CActiveEdge>();
+
+            BuildEdgeList(shape.Vertices, ref et);
+
+            for (int i = rec.Y - rec.Height; i < rec.Y; i++)
+            {
+                buildActiveList(ref active, ref et[i]);
+                if (active.Count != 0)
+                {
+                    FillScan(i, ref active,layer,FillColor);
+                    updateEdgeList(i, ref active);
+                    active.Sort();
+                }
+            }
+        }
+
+        private void ScanLineFillCircle(ref Layer layer, Circle shape,Color FillColor)
+        {
+            var o = shape.Vertices[0];
+            var r = shape.Radius;
+
+            int x = 0, y = (int)r;
+            int c1 = x << 1, c2 = y << 1;
+            int p = 1 - y;
+
+            while (x < y)
+            {
+                Fill4Line(o.X, o.Y, x, y, layer, FillColor);
+
+                x++;
+                c1 += 2;
+                if (p < 0)
+                    p += c1 + 1;
+                else
+                {
+                    y--;
+                    c2 -= 2;
+                    p += c1 - c2 + 1;
+                }
+            }
+            Fill4Line(o.X, o.Y, x, y, layer, FillColor);
+        }
+
+        private void ScanLineFillEllipse(ref Layer layer, Ellipse shape, Color FillColor)
+        {
+            int rx = shape.MajorAxis, ry = shape.MinorAxis;
+            var o = new Point((shape.EndVertex.X + shape.StartVertex.X) / 2,
+                (shape.EndVertex.Y + shape.StartVertex.Y) / 2);
+
+            int x = 0, y = ry;
+            int c1 = 2 * ry * ry * x, c2 = 2 * rx * rx * y;
+            float p = ry * ry - rx * rx * ry + 0.25F * rx * rx;
+
+            while (c1 < c2)
+            {
+                Fill2Line(o.X, o.Y, x, y,layer,FillColor);
+
+                x++;
+                if (p < 0)
+                {
+                    c1 += 2 * ry * ry;
+                    p += c1 + ry * ry;
+                }
+                else
+                {
+                    y--;
+                    c1 += 2 * ry * ry;
+                    c2 -= 2 * rx * rx;
+                    p += c1 - c2 + ry * ry;
+                }
+            }
+
+            c1 = 2 * rx * rx * y;
+            c2 = 2 * ry * ry * x;
+            p = ry * ry * (x + 0.5F) * (x + 0.5F) + rx * rx * (y - 1) * (y - 1) - rx * rx * ry * ry;
+
+            while (y != 0)
+            {
+                Fill2Line(o.X, o.Y, x, y, layer, FillColor);
+
+                y--;
+                if (p > 0)
+                {
+                    c1 -= 2 * rx * rx;
+                    p += rx * rx - c1;
+                }
+                else
+                {
+                    x++;
+                    c1 -= 2 * rx * rx;
+                    c2 += 2 * ry * ry;
+                    p += c2 - c1 + rx * rx;
+                }
+            }
+            Fill2Line(o.X, o.Y, x, y, layer, FillColor);
         }
 
         private void SetColorToArray(ref byte[] arr, int x, int y, int stride, CColor c)
@@ -210,6 +360,121 @@ namespace TabletC.DrawPad
                     break;
             }
             return ret;
+        }
+
+        private void BuildEdgeList(IList<Point> points, ref SortedDoublyLinkedList<CActiveEdge>[] et)
+        {
+            var cnt = points.Count;
+            int i, yPrev = points[cnt - 2].Y;
+
+            Point v1 = points[cnt - 1];
+            for (i = 0; i < cnt; i++)
+            {
+                Point v2 = points[i];
+                if (v1.Y != v2.Y)
+                {
+                    // Nonhorizontal line
+                    if (v1.Y < v2.Y) // up-going edge
+                        makeEdgeRec(ref v1, ref v2, yNext(i, cnt, ref points), ref et);
+                    else             // down-going edge
+                        makeEdgeRec(ref v2, ref v1, yPrev, ref et);
+                }
+                yPrev = v1.Y;
+                v1 = v2;
+            }
+        }
+
+        private void buildActiveList(ref SortedDoublyLinkedList<CActiveEdge> dest, ref SortedDoublyLinkedList<CActiveEdge> source)
+        {
+            foreach (var edge in source)
+                dest.Add(edge);
+        }
+
+        private void FillScan(int line, ref SortedDoublyLinkedList<CActiveEdge> ae,Layer layer,Color FillColor)
+        {
+            var points = new CActiveEdge[2];
+            int i = 0;
+
+            foreach (var edge in ae)
+            {
+                points[i] = edge;
+
+                if (i == 1)
+                    FillLine((int)Math.Round(points[0].XIntersection, 0), (int)Math.Round(points[1].XIntersection, 0), line,layer,FillColor);
+
+                i = (i + 1) % 2;
+            }
+        }
+
+        private void FillLine(int x1, int x2, int y,Layer layer,Color FillColor)
+        {
+            for (var j = x1; j < x2; j++) 
+                layer.ImageBuffer.SetPixel(j, y, FillColor);
+                //_graphic.Vertex(j, y);
+        }
+
+        private void updateEdgeList(int line, ref SortedDoublyLinkedList<CActiveEdge> ae)
+        {
+            var p = ae.First;
+
+            while (p != null)
+            {
+                if (line >= p.Value.YUper)
+                {
+                    ae.Remove(p);
+                }
+                else
+                    p.Value.XIntersection += p.Value.ReciSlope;
+                p = p.Next;
+            }
+        }
+
+        private void makeEdgeRec(ref Point lower, ref Point upper, int yComp, ref SortedDoublyLinkedList<CActiveEdge>[] et)
+        {
+            var ae = new CActiveEdge
+            {
+                ReciSlope = (float)(upper.X - lower.X) / (upper.Y - lower.Y),
+                XIntersection = lower.X
+            };
+
+            // Make shorter edge
+            if (upper.Y < yComp)
+                ae.YUper = upper.Y - 1;
+            else
+                ae.YUper = upper.Y;
+            et[lower.Y].Add(ae);
+        }
+
+        private int yNext(int k, int cnt, ref IList<Point> points)
+        {
+            int j;
+
+            if (k + 1 > cnt - 1)
+                j = 0;
+            else
+                j = k + 1;
+
+            while (points[k].Y == points[j].Y)
+                if (j + 1 > cnt - 1)
+                    j = 0;
+                else
+                    j++;
+
+            return points[j].Y;
+        }
+
+        private void Fill4Line(int xc, int yc, int x, int y,Layer layer,Color FillColor)
+        {
+            FillLine(-x + xc, x + xc, y + yc, layer, FillColor);
+            FillLine(-y + xc, y + xc, x + yc, layer, FillColor);
+            FillLine(-y + xc, y + xc, -x + yc, layer, FillColor);
+            FillLine(-x + xc, x + xc, -y + yc, layer, FillColor);
+        }
+
+        private void Fill2Line(int xc, int yc, int x, int y, Layer layer, Color FillColor)
+        {
+            FillLine(-x + xc, x + xc, y + yc, layer, FillColor);
+            FillLine(-x + xc, x + xc, -y + yc, layer, FillColor);
         }
     }
 }
